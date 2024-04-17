17c8b6e3618d34a92430b6d716827b9efa101e1c
jan hach <jan.hach@outlook.de>
2024-04-17 22:19:03 +0200

    Moved css, js and images into their own folders

--- pblsh.giraffe/Components.fs
+++ pblsh.giraffe/Components.fs
@@ -1,50 +1,49 @@
 ﻿module pblsh.Components
 
+open System.Xml
 open Giraffe.ViewEngine.Attributes
 open Giraffe.ViewEngine.HtmlElements
 open pblsh.Models
-        
-let titledLayoutCJ (pageTitle: string) (cssFiles: string list) (jsFiles: string list) (content: XmlNode list) =
+
+let titledLayoutCssJs (cssFiles: string list) (jsFiles: string list) (pageTitle: string) (content: XmlNode list) =
     html
         []
         [ head
               []
               [ title [] [ encodedText pageTitle ]
-                link [ _rel "stylesheet"; _type "text/css"; _href "/pblsh.reboot.css" ]
-                link [ _rel "stylesheet"; _type "text/css"; _href "/pblsh.css" ]
+                link [ _rel "stylesheet"; _type "text/css"; _href "/css/pblsh.reboot.css" ]
+                link [ _rel "stylesheet"; _type "text/css"; _href "/css/pblsh.css" ]
                 yield!
                     cssFiles
-                    |> List.map (fun f -> link [ _rel "stylesheet"; _type "text/css"; _href (sprintf "/%s" f) ])
-              ]
-          body [] content
-          yield!
-            jsFiles
-            |> List.map (fun f -> script [ _type "text/javascript"; _src (sprintf "/%s" f) ] [])
-        ]         
-        
-let titledLayout (pageTitle: string) =
-    titledLayoutCJ pageTitle [] []
+                    |> List.map (fun f -> link [ _rel "stylesheet"; _type "text/css"; _href (sprintf "/css/%s" f) ])
+                yield!
+                    jsFiles
+                    |> List.map (fun f -> script [ _type "text/javascript"; _src (sprintf "/js/%s" f) ] []) ]
+          body [] content ]
 
-let titledLayoutCss (pageTitle: string) (cssFiles: string list) =
-    titledLayoutCJ pageTitle cssFiles []
-        
-let titledLayoutJs (pageTitle: string) (jsFiles: string list) =
-    titledLayoutCJ pageTitle [] jsFiles
+let titledLayout = titledLayoutCssJs [] []
+
+let titledLayoutCss (cssFiles: string list) = titledLayoutCssJs cssFiles []
+
+let titledLayoutJs (jsFiles: string list) = titledLayoutCssJs [] jsFiles
 
 let layout (content: XmlNode list) = titledLayout "pblsh" content
 
-let partial () =
+let topRow (middle: XmlNode list) (right: XmlNode list) =
     header
         []
         [ div [ _id "logo" ] [ a [ _href "/index" ] [ encodedText "pblsh" ] ]
-          input [ _id "search"; _type "input"; _placeholder "Search..." ]
-          div
-              [ _id "header-right" ]
-              [ div [ _class "action" ] [ a [ _id "login"; _href "/account/login" ] [ encodedText "Log in" ] ]
-                div [ _class "filled-action" ] [ a [ _id "signup"; _href "/account/signup" ] [ encodedText "Sign up" ] ] ] ]
+          div [ _id "header-middle" ] middle
+          div [ _id "header-right" ] right ]
 
-let emptyPartial () =
-    header [] [ div [ _id "logo" ] [ a [ _href "/index" ] [ encodedText "pblsh" ] ] ]
+let filledTopRow () =
+    topRow
+        [ input [ _id "search"; _type "input"; _placeholder "Search..." ] ]
+        [ div [ _class "action" ] [ a [ _id "login"; _href "/account/login" ] [ encodedText "Log in" ] ]
+          div [ _class "filled-action" ] [ a [ _id "signup"; _href "/account/signup" ] [ encodedText "Sign up" ] ] ]
+
+
+let emptyTopRow () = topRow [] []
 
 let private routeElement (routePart: RoutePart) =
     div
@@ -53,7 +52,17 @@ let private routeElement (routePart: RoutePart) =
           img [ _src "/icons/chevron.svg"; _height "14" ] ]
 
 let navigation (route: RoutePart list) =
-    div [ _id "navigation" ] [ yield! route |> List.map routeElement ]
+    div [ _id "navigation" ] (route |> List.map routeElement)
 
 let dot str =
     a [ _class "tag"; _href (sprintf "/dots/%s" str) ] [ encodedText (sprintf ".%s" str) ]
+
+let dialogCssJs (cssFiles: string list) (jsFiles: string list) (pageTitle: string) (content: XmlNode list) =
+    [ emptyTopRow (); main [ _class "shiny" ] [ div [ _id "center" ] content ] ]
+    |> titledLayoutCssJs ("dialog.css" :: cssFiles) ("mousetracker.js" :: jsFiles) pageTitle
+
+let dialogCss cssFiles = dialogCssJs cssFiles []
+
+let dialogJs jsFiles = dialogCssJs [] jsFiles
+
+let dialog = dialogCssJs [] []
--- pblsh.giraffe/Views.fs
+++ pblsh.giraffe/Views.fs
@@ -22,64 +22,52 @@ let randomPostCard _ =
 let index () =
     let elements = List.ofSeq { 1..10 }
 
-    [ partial ()
+    [ filledTopRow ()
       navigation [ { Text = "Home"; Link = "/index" } ]
       main [] [ div [] [ yield! elements |> List.map randomPostCard ] ] ]
-    |> titledLayoutCss "pblsh" [ "index.css" ]
+    |> titledLayoutCss [ "index.css" ] "pblsh"
 
 let login () =
-    [ emptyPartial ()
-      main
+    [ form
+          [ _action "login"; _method "post" ]
+          [ h1 [] [ encodedText "Log in" ]
+            label [ _for "email" ] [ encodedText "E-Mail" ]
+            input [ _id "email"; _type "text"; _name "email"; _placeholder "E-Mail" ]
+            label [ _for "password" ] [ encodedText "Password" ]
+            input [ _id "password"; _type "password"; _name "password"; _placeholder "Password" ]
+            input [ _type "submit"; _value "Log in"; _class "filled-action" ] ]
+      div
           []
-          [ div
-                [ _id "center" ]
-                [ form
-                      [ _action "login"; _method "post" ]
-                      [ h1 [] [ encodedText "Log in" ]
-                        label [ _for "email" ] [ encodedText "E-Mail" ]
-                        input [ _id "email"; _type "text"; _name "email"; _placeholder "E-Mail" ]
-                        label [ _for "password" ] [ encodedText "Password" ]
-                        input [ _id "password"; _type "password"; _name "password"; _placeholder "Password" ]
-                        input [ _type "submit"; _value "Log in"; _class "filled-action" ] ]
-                  div
-                      []
-                      [ span [] [ encodedText "Don't have an account? " ]
-                        a [ _class "action-link"; _href "/account/signup" ] [ encodedText "Sign up" ] ] ] ] ]
-    |> titledLayoutCss "pblsh.login" [ "login.css" ]
+          [ span [] [ encodedText "Don't have an account? " ]
+            a [ _class "action-link"; _href "/account/signup" ] [ encodedText "Sign up" ] ] ]
+    |> dialog "pblsh.login"
 
 let signup () =
-    [ emptyPartial ()
-      main
-          [ _class "shiny" ]
-          [ div
-                [ _id "center" ]
-                [ form
-                      [ _action "signup"; _method "post" ]
-                      [ h1 [] [ encodedText "Sign up" ]
-                        label [ _for "email" ] [ encodedText "E-Mail" ]
-                        input [ _id "email"; _type "text"; _name "email"; _placeholder "E-Mail" ]
-                        label [ _for "password" ] [ encodedText "Password" ]
-                        input [ _id "password"; _type "password"; _name "password"; _placeholder "Password" ]
-                        label [ _for "username" ] [ encodedText "Username" ]
-                        input [ _id "username"; _type "text"; _name "username"; _placeholder "Username" ]
-                        input [ _type "submit"; _value "Sign up"; _class "filled-action" ] ]
-                  div
-                      []
-                      [ span [] [ encodedText "Already signed up? " ]
-                        a [ _class "action-link"; _href "/account/login" ] [ encodedText "Log in" ] ] ] ] ]
-    |> titledLayoutCJ "pblsh.signup" [ "login.css" ] [ "mousetracker.js" ]
+    [ form
+          [ _action "signup"; _method "post" ]
+          [ h1 [] [ encodedText "Sign up" ]
+            label [ _for "email" ] [ encodedText "E-Mail" ]
+            input [ _id "email"; _type "text"; _name "email"; _placeholder "E-Mail" ]
+            label [ _for "password" ] [ encodedText "Password" ]
+            input [ _id "password"; _type "password"; _name "password"; _placeholder "Password" ]
+            label [ _for "username" ] [ encodedText "Username" ]
+            input [ _id "username"; _type "text"; _name "username"; _placeholder "Username" ]
+            input [ _type "submit"; _value "Sign up"; _class "filled-action" ] ]
+      div
+          []
+          [ span [] [ encodedText "Already signed up? " ]
+            a [ _class "action-link"; _href "/account/login" ] [ encodedText "Log in" ] ] ]
+    |> dialog "pblsh.signup"
 
 let errorWithRedirect (link: string) =
-    [ emptyPartial ()
-      main [] [ div [ _id "center" ] [ h1 [] [ encodedText "Something happened" ] ] ] ]
-    |> titledLayoutCss "pblsh.error" [ "error.css" ]
+    [ div
+          [ _id "frame" ]
+          [ img [ _src "/images/error.jpg" ]
+            h1 [] [ encodedText "You summoned an octopus!" ]
+            div [ _id "bottom-text" ] [ encodedText "An error occurred. Please try again another time." ] ] ]
+    |> dialogCss [ "error.css" ] "pblsh.error"
 
 let confirmEmail () =
-    [ emptyPartial ()
-      main
-          []
-          [ div
-                [ _id "center" ]
-                [ h1 [] [ encodedText "Thanks for signing up!" ]
-                  div [] [ encodedText "We send you an email to confirm. Please enter the code below:" ] ] ] ]
-    |> titledLayoutCss "pblsh.confirm" [ "confirm.css" ]
+    [ h1 [] [ encodedText "Thanks for signing up!" ]
+      div [] [ encodedText "We send you an email to confirm. Please enter the code below:" ] ]
+    |> dialog "pblsh.confirm"
