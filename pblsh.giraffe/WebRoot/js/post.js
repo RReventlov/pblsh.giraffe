3c4430b7ced9823ffb9b22cb0409d8b6578c9e6b
Lars Richter <larsrichter41@gmail.com>
2024-05-23 14:24:36 +0200

    Fixed Bug where Show/Hide would not work when not signed in

--- pblsh.giraffe/WebRoot/js/post.js
+++ pblsh.giraffe/WebRoot/js/post.js
@@ -1,17 +1,4 @@
-var commentButton = document.getElementById("writeComment");
-var commentForm = document.getElementById("newComment");
-commentButton.addEventListener("click", function () {
-   commentButton.setAttribute("style","display:none");
-   commentForm.setAttribute("style","");
-})
-const replyButtons = document.querySelectorAll('.replyButton');
-replyButtons.forEach(function (button) {
-   button.addEventListener('click', function () {
-      const form = this.nextElementSibling;
-      this.style.display = 'none';
-      form.style.display = 'block';
-   });
-});
+
 const hideButtons = document.querySelectorAll('.hideButton');
 hideButtons.forEach(function (button) {
    button.addEventListener('click', function () {
@@ -27,5 +14,19 @@ showButtons.forEach(function (button) {
       const comment = this.parentElement.nextElementSibling;
       this.style.display = 'none';
       comment.style.display = 'block';
+   });
+});
+var commentButton = document.getElementById("writeComment");
+var commentForm = document.getElementById("newComment");
+commentButton.addEventListener("click", function () {
+   commentButton.setAttribute("style","display:none");
+   commentForm.setAttribute("style","");
+})
+const replyButtons = document.querySelectorAll('.replyButton');
+replyButtons.forEach(function (button) {
+   button.addEventListener('click', function () {
+      const form = this.nextElementSibling;
+      this.style.display = 'none';
+      form.style.display = 'block';
    });
 });
